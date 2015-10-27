9be34a558fdc09a62f8e309679188c8c20b1f702
root <g.almeidaa.campos@gmail.com>
2015-10-27 16:48:09 -0200

    Adicionei o modelo Sector

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20151026152216) do
-
+ActiveRecord::Schema.define(version: 20151027182842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "name"
-  end
   create_table "jobs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sectors", force: :cascade do |t|
+    t.string   "short_name"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "type_activities", force: :cascade do |t|
