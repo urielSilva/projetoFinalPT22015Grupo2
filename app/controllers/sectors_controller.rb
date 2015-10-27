b899728725f4b06d9c0160561e6e21d3c47d022c
Rafael Alves Fernandes <rafaelalves@cjr.org.br>
2015-10-27 19:23:47 -0200

    Consertando sectors/jobs

--- app/controllers/sectors_controller.rb
+++ app/controllers/sectors_controller.rb
@@ -4,7 +4,7 @@ class SectorsController < ApplicationController
   # GET /sectors
   # GET /sectors.json
   def index
-    @sectors = Sector.where(name: "wololo")
+    @sectors = Sector.all
   end
 
   # GET /sectors/1
