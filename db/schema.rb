4d61ba6e95cdadcb3d74fc76b38f65f3a6527b96
Rafael Alves Fernandes <rafaelalves@cjr.org.br>
2015-10-27 16:34:34 -0200

    validates

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20151026152216) do
-
+ActiveRecord::Schema.define(version: 20151026154208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "name"
-  end
-  create_table "jobs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
