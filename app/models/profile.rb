642d45c774fcd06dbb7095b0361fa63ae916a8b4
annecarolina <annecarolina@cjr.org.br>
2015-11-03 13:30:41 -0200

    validando profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,3 @@
 class Profile < ActiveRecord::Base
+	validates_presence_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   resources :knowledge_levels
   resources :type_activities
-  resources :profiles
+ # resources :profiles
   resources :jobs
+
+  get "/profiles" => "profiles#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
