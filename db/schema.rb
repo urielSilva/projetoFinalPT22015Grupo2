330ba024b2132062efc48f8f9540bcd2648e90b4
gabriel <g.almeidaa.campos@gmail.com>
2015-11-11 20:37:23 -0200

    Adicionado o model referente a Areas

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105152638) do
+ActiveRecord::Schema.define(version: 20151111223454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
-
-    t.string   "description"
-    t.integer  "credits"
-    t.string   "type"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "areas", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "sector_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  add_index "areas", ["sector_id"], name: "index_areas_on_sector_id", using: :btree
 
   create_table "jobs", force: :cascade do |t|
     t.string   "name"
@@ -37,24 +37,6 @@ ActiveRecord::Schema.define(version: 20151105152638) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-  create_table "members", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "members", ["email"], name: "index_members_on_email", unique: true, using: :btree
-  add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -122,6 +104,7 @@ ActiveRecord::Schema.define(version: 20151105152638) do
   add_index "users", ["profile_id"], name: "index_users_on_profile_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "areas", "sectors"
   add_foreign_key "projects", "project_statuses"
   add_foreign_key "users", "jobs"
   add_foreign_key "users", "profiles"
