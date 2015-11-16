f45ccd6344e24eeba430212c15a6e1b8fe83c296
Rafael <rafaelalves@cjr.org.br>
2015-11-16 11:15:36 -0200

    novo estilo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require materialize-sprockets
+//= require jquery2
+//= require jquery_ujs
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151111223454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
-    t.string   "activity_description"
-    t.integer  "activity_credit_number"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-  
   create_table "areas", force: :cascade do |t|
     t.string   "name"
     t.integer  "sector_id"
