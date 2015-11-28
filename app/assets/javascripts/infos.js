6b2702c5b23de7da3e4903ec7069b732eca1ce56
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2015-11-28 14:13:04 -0200

    mudanças gerais ai

--- app/assets/javascripts/infos.js
+++ app/assets/javascripts/infos.js
@@ -58,6 +58,21 @@ $(document).ready(function () {
     //grafico de membros por nucleo
     $('#MembrosKer').click(function () {
         $.getJSON('/membro_nucleo', function(json) {
+
+             // Radialize the colors
+    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    });
     
          $('#container').highcharts({
         chart: {
