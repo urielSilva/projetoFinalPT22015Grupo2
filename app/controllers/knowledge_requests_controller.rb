b46cce0e52dfae8fd6e50bd59b0aca17799a3178
Rafael <rafaelalves@cjr.org.br>
2015-12-08 13:49:24 -0200

    correcoes

--- app/controllers/knowledge_requests_controller.rb
+++ app/controllers/knowledge_requests_controller.rb
@@ -1,34 +1,29 @@
 class KnowledgeRequestsController < ApplicationController
-  before_action :set_knowledge_request, only: [:show, :edit, :update, :destroy]
 
-  # GET /knowledge_requests
-  # GET /knowledge_requests.json
+  before_action :authenticate_user!
+  before_action :set_knowledge_request, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource except: [:create]
+
   def index
     @knowledge_requests = KnowledgeRequest.all
   end
 
-  # GET /knowledge_requests/1
-  # GET /knowledge_requests/1.json
   def show
   end
 
-  # GET /knowledge_requests/new
   def new
     @knowledge_request = KnowledgeRequest.new
   end
 
-  # GET /knowledge_requests/1/edit
   def edit
   end
 
-  # POST /knowledge_requests
-  # POST /knowledge_requests.json
   def create
     @knowledge_request = KnowledgeRequest.new(knowledge_request_params)
 
     respond_to do |format|
       if @knowledge_request.save
-        format.html { redirect_to @knowledge_request, notice: 'Knowledge request was successfully created.' }
+        format.html { redirect_to @knowledge_request, notice: 'A requisição de conhecimento foi feita.'}
         format.json { render :show, status: :created, location: @knowledge_request }
       else
         format.html { render :new }
@@ -37,12 +32,10 @@ class KnowledgeRequestsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /knowledge_requests/1
-  # PATCH/PUT /knowledge_requests/1.json
   def update
     respond_to do |format|
       if @knowledge_request.update(knowledge_request_params)
-        format.html { redirect_to @knowledge_request, notice: 'Knowledge request was successfully updated.' }
+        format.html { redirect_to @knowledge_request, notice: 'A requisição de conhecimento foi atualizada com sucesso.' }
         format.json { render :show, status: :ok, location: @knowledge_request }
       else
         format.html { render :edit }
@@ -51,24 +44,22 @@ class KnowledgeRequestsController < ApplicationController
     end
   end
 
-  # DELETE /knowledge_requests/1
-  # DELETE /knowledge_requests/1.json
   def destroy
     @knowledge_request.destroy
     respond_to do |format|
-      format.html { redirect_to knowledge_requests_url, notice: 'Knowledge request was successfully destroyed.' }
+      format.html { redirect_to knowledge_requests_url, notice: 'A requisição de conhecimento foi recusada.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_knowledge_request
       @knowledge_request = KnowledgeRequest.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def knowledge_request_params
       params.require(:knowledge_request).permit(:knowledge_id, :user_id)
     end
+
 end
--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -1,78 +1,100 @@
 class KnowledgesController < ApplicationController
 
   before_action :authenticate_user!
-	before_action :set_knowledge, only: [:show, :edit, :update, :destroy]
+  before_action :set_knowledge, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource except: [:create]
 
-	def index
+  # Na index, como há um formulário de busca, é necessário filtrar os resultados a partir dos parâmetros enviados do formulário
+  # A paginação é feita de 7 em 7 registros
+  def index
     @search = Knowledge.ransack(params[:q])
-    @knowledges = @search.result(distinct: true).paginate(page: params[:page], per_page: 7)
+    @knowledges = @search.result(distinct: true).includes(:technology, :knowledge_level).paginate(page: params[:page], per_page: 7)
   end
 
-	def new
-		@knowledge = Knowledge.new
-	end
+  def new
+    @knowledge = Knowledge.new
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def create
-		@knowledge = Knowledge.new(knowledge_params)
+  def create
+    @knowledge = Knowledge.new(knowledge_params)
 
-		respond_to do |format|
-			if @knowledge.save
-				format.html { redirect_to @knowledge, notice: 'Knowledge was successfully created.' }
-      		format.json { render :show, status: :created, location: @knowledge }
-      	else
-      		format.html { render :new }
-      		format.json { render json: @knowledge_level, status: :unprocessable_entity }
-      	end
+    respond_to do |format|
+      if @knowledge.save
+        format.html { redirect_to @knowledge, notice: 'O conhecimento foi criado com sucesso.' }
+          format.json { render :show, status: :created, location: @knowledge }
+        else
+          format.html { render :new }
+          format.json { render json: @knowledge_level, status: :unprocessable_entity }
+        end
       end
     end
 
   def update
-  	respond_to do |format|
-  		if @knowledge.update(knowledge_params)
-  			format.html { redirect_to @knowledge, notice: 'Knowledge was succesfully formated' }
-  			format.json { render :show, status: :ok, location: @knowledge }
-  		else
-  			format.html { render :edit }
-      		format.json { render json: @knowledge.errors, status: :unprocessable_entity }
-      	end
+    respond_to do |format|
+      if @knowledge.update(knowledge_params)
+        format.html { redirect_to @knowledge, notice: 'O conhecimento foi atualizado com sucesso.' }
+        format.json { render :show, status: :ok, location: @knowledge }
+      else
+        format.html { render :edit }
+          format.json { render json: @knowledge.errors, status: :unprocessable_entity }
+        end
       end
   end
 
   def destroy 
-  	@knowledge.destroy
-  	respond_to do |format|
-  		format.html { redirect_to knowledges_url, notice: 'Knowledge was succesfully destroyed' }
-  		format.json { head :no_content }
-  	end
+    @knowledge.destroy
+    respond_to do |format|
+      format.html { redirect_to knowledges_url, notice: 'O conhecimento foi excluído com sucesso.' }
+      format.json { head :no_content }
+    end
   end
 
+  # Esse método cria uma requisição de conhecimento do usuário atual e o conhecimento escolhido
+  # O administrador é definido antes de se criar a requisição, para que o conhecimento seja diretamente associado a ele
   def requisitar
-    @knowledge_request = KnowledgeRequest.new(user_id: current_user.id, knowledge_id: @knowledge.id)
 
-    respond_to do |format|
-      if @knowledge_request.save
-        KnowledgeRequestMailer.request_email(current_user, @knowledge_request).deliver_now
-        current_user.send_message(User.find(7), "Requisitei um conhecimento, abs", "Requisição de Conhecimento")
-        format.html { redirect_to knowledges_url, notice: 'A solicitação foi feita ao administrador.' }
-        format.json { render :show, status: :created, location: @knowledge }
-      else
-        format.html { render :new }
-        format.json { render json: @knowledge.errors, status: :unprocessable_entity }
+    # Define um administrador
+    @administrador = User.find(7)
+    
+    # Se o administrador for igual ao usuário atual
+    if @administrador == current_user
+      # Associa conhecimento diretamente
+
+      respond_to do |format|
+        format.html {redirect_to knowledges_url, notice: 'O conhecimento foi associado com sucesso.'}
       end
+
+    else 
+      # Senão cria uma requisição para que o administrador possa aprovar
+      @knowledge_request = KnowledgeRequest.new(user_id: current_user.id, knowledge_id: @knowledge.id)
+
+      respond_to do |format|
+        if @knowledge_request.save
+          # Comente a linha abaixo caso esteja usando rede WiFi da UnB (UNB Wireless ou eduroam)
+          # KnowledgeRequestMailer.request_email(current_user, @knowledge_request).deliver_now
+          # Envia uma mensagem pelo sistema interno de mensagens, informando ao administrador que foi solicitado um novo conhecimento
+          current_user.send_message(User.find(7), "Requisitei o conhecimento: " + @knowledge_request.knowledge.description + " (" + @knowledge_request.knowledge.knowledge_level.level + "). Aprove ou recuse acessando Requisições de Conhecimento da sua home.", "[CONHECIMENTO] Requisição")
+          format.html { redirect_to knowledges_url, notice: 'A solicitação foi feita ao administrador.' }
+          format.json { render :show, status: :created, location: @knowledge }
+        else
+          format.html { render :new }
+          format.json { render json: @knowledge.errors, status: :unprocessable_entity }
+        end
+      end
+
     end
 
   end
 
   private
-  	def set_knowledge
-  		@knowledge = Knowledge.find(params[:id])
-  	end
+    def set_knowledge
+      @knowledge = Knowledge.find(params[:id])
+    end
 
-  	def knowledge_params
-  		params.require(:knowledge).permit(:knowledge_level_id, :description, :technology_id)
-  	end
+    def knowledge_params
+      params.require(:knowledge).permit(:knowledge_level_id, :description, :technology_id)
+    end
 end
